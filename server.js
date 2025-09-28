65110dfc5b4722d65e275752bbdbc92e96bd10f6
bdenesh123 <drajan@wvb.com>
2025-09-28 18:48:33 +0800

    test6

--- server.js
+++ server.js
@@ -9,13 +9,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+const corsOptions = {
+  origin: "https://athena-ai-project.vercel.app",
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 
